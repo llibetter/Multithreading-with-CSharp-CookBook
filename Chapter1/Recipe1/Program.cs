7afbaeceda1667460fa3dde9a1f2c7983f902100
lli <915133364@qq.com>
2019-10-24 20:30:47 +0800

    第一次提交

---
+++ Chapter1/Recipe1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace Chapter1.Recipe1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread t = new Thread(PrintNumbers);
+			t.Start();
+			PrintNumbers();
+            Console.ReadKey();
+		}
+
+		static void PrintNumbers()
+		{
+			WriteLine("Starting...");
+			for (int i = 1; i < 10; i++)
+			{
+               // Thread.Sleep(500);
+				WriteLine(i);
+			}
+		}
+	}
+}
---
+++ Chapter5/Recipe2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Chapter5.Recipe2
+{
+	class Program
+	{
+		static  void Main(string[] args)
+		{
+            WriteLine("主线程1");
+            Task t = AsynchronousProcessing();
+            WriteLine("主线程2");
+			t.Wait();
+		}
+
+		static async Task AsynchronousProcessing()
+		{
+			Func<string, Task<string>> asyncLambda = async name => {
+				await Task.Delay(TimeSpan.FromSeconds(2));
+                WriteLine("主线程5");
+
+                return
+                    $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}." +
+				    $" Is thread pool thread: {CurrentThread.IsThreadPoolThread}";
+			};
+            WriteLine("主线程3");
+            string result = await asyncLambda("async lambda");
+            WriteLine("主线程4");
+            WriteLine(result);
+		}
+	}
+}
